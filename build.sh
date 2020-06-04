e5718fd8c290b942a092cb6e4d869648ee95b115
root <ismael.darosa@gmail.com>
2020-06-04 19:07:51 -0300

    finalizacion build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 #construimos las imágenes a partir de 2 dockerfile que levantamos desde github
-docker build  -t dev/challenge https://github.com/ismaeldarosa19/challengedeploy.git -f dev/dockerfile.dev
-docker build  -t mysql/challenge https://github.com/ismaeldarosa19/challengedeploy.git -f database/dockerfile.mysql
+#docker build  -t dev/challenge https://github.com/ismaeldarosa19/challengedeploy.git -f dev/dockerfile.dev
+#docker build  -t mysql/challenge https://github.com/ismaeldarosa19/challengedeploy.git -f database/dockerfile.mysql
 
 #creamos la funcion randomstring para generar contraseñas randómicas para las credenciales de la DB
 randomstring()
@@ -16,18 +17,35 @@ var_mysql_password=$(randomstring)
 var_mysql_database='challenge'
 
 #ejecutamos el contenedor mysql, asignamos las variables generadas a las variables de entorno que utiliza el dockerfile para crear la DB
-docker run -it -e MYSQL_ROOT_PASSWORD=$var_mysql_root_password -e MYSQL_USER=$var_mysql_user -e MYSQL_PASSWORD=$var_mysql_password -e MYSQL_DATABASE=$var_mysql_database --name mysql -d mysql/challenge
+#docker run -it -e MYSQL_ROOT_PASSWORD=$var_mysql_root_password -e MYSQL_USER=$var_mysql_user -e MYSQL_PASSWORD=$var_mysql_password -e MYSQL_DATABASE=$var_mysql_database --name mysql -d mysql/challenge
 
 #ejecutamos el contenedor dev y lo linkeamos a mysql para poder conectarnos a la base de datos
-docker run -e userDB=$var_mysql_user -e passDB=$var_mysql_password -it --name dev --link mysql -d dev/challenge
+#docker run -e userDB=$var_mysql_user -e passDB=$var_mysql_password -it --name dev --link mysql -d dev/challenge
 
 #mensaje de finalización y ayuda de ejecución
-#clear
-#sleep 2
-COLOR1='\033[0;33m'
-COLOR2='\033[0;93m'
+sleep 1
+clear
+COLOR1='\033[0;32m'
+COLOR2='\033[1;92m'
 COLOR0='\033[0m'
+
+y=2
+x=0
+i=0
+while [ $i -lt 101 ]; do
+    tput cup $y $x
+    echo ''
+    echo ''
+    echo ''
+    echo '=====Finalizando deploy ==== API Gmail Python====='
+    echo $i%                                           
+    echo '=================Ismael da Rosa==================='
+    echo ''
+    i=$((i+5))
+sleep 1
+done
+
 printf "${COLOR1}El deploy ha finalizado.\n"
-echo
 printf "Ejecutar: ${COLOR2}docker exec -it dev python challengeGmail.py\n${COLOR0}"
+echo '\n\n\n'
 
